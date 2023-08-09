4439d03ee9cfbf7151ecf2342f805888fbf48daf
agibeault <alexandre.gibeault@powerside.com>
2023-08-08 20:42:47 -0400

    remove unused import

--- src/main/java/com/loopingz/SesSmtpRelay.java
+++ src/main/java/com/loopingz/SesSmtpRelay.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.IOUtils;
 
